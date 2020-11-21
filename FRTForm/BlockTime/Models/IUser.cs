f6b9c0d82aa6e72df2bb7380ee6f8fdf558951c4
Tony Horsham <horshamt@gmail.com>
2020-11-21 16:10:20 +1100

    add UserSettings to demo and IUser to FRTForm

---
+++ FRTForm/BlockTime/Models/IUser.cs
@@ -0,0 +1,10 @@
+﻿// 21 11 2020 Created by Tony Horsham 16:08
+
+namespace FRTForm.BlockTime.Models
+{
+    public interface IUser
+    {
+        public string UserId { get; }
+        public string UserName { get; }
+    }
+}
