f4a08509a7513d7e40342b34e64f95120895cb58
Tony Horsham <horshamt@gmail.com>
2020-11-21 16:04:22 +1100

    add FRTFormSettings

---
+++ Demo/Settings/FRTFormSettings.cs
@@ -0,0 +1,18 @@
+﻿// 21 11 2020 Created by Tony Horsham 16:01
+// Copyright T & D H Family Trust
+
+using System.Collections.Generic;
+using FRTForm.Models;
+
+namespace Demo.Settings
+{
+    public readonly struct FRTFormSettings
+    {
+        public FRTFormSettings(Dictionary<string, IFormSpecs> formSpecs)
+        {
+            FormSpecs = formSpecs;
+        }
+
+        public Dictionary<string, IFormSpecs> FormSpecs { get; }
+    }
+}
