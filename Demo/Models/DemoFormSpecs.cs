4d6d22648a9691bcf90bf25b201a8f59dd1e1ef9
Tony Horsham <horshamt@gmail.com>
2020-11-21 15:58:46 +1100

    add DemoFormSpecs

---
+++ Demo/Models/DemoFormSpecs.cs
@@ -0,0 +1,24 @@
+﻿// 21 11 2020 Created by Tony Horsham 15:56
+
+using System.Collections.Generic;
+using FRTForm.Models;
+using FRTForm.Utilities;
+
+namespace Demo.Models
+{
+    public class DemoFormSpecs : IFormSpecs
+    {
+        public DemoFormSpecs(int labelWidth, List<IFormElement> elements,
+            IFormProcessor formProcessor)
+        {
+            LabelWidth = labelWidth;
+            Elements = elements;
+            FormProcessor = formProcessor;
+        }
+
+        public string FormId => "demoForm";
+        public int LabelWidth { get; set; }
+        public List<IFormElement> Elements { get; }
+        public IFormProcessor FormProcessor { get; }
+    }
+}
