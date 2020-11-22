e4dfcf6dfaffe080f300458b18d7834f01e69862
Tony Horsham <horshamt@gmail.com>
2020-11-22 17:24:01 +1100

    add dummy services

---
+++ Demo/FormClasses/Services/DummyEmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FRTForm.Services;
+
+namespace Common.Services
+{
+    public class DummyEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demo/FormClasses/Services/DummySmsSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FRTForm.Services;
+
+namespace Common.Services
+{
+    public class DummySmsSender : ISmsSender
+    {
+        public Task SendSmsAsync(string number, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
