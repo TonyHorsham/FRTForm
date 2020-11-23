71259b1d5a8b7775d79ce3a790040a060abf941c
Tony Horsham <horshamt@gmail.com>
2020-11-23 16:25:23 +1100

    add AllSettings and use in Index to complete setup

---
+++ Demo/FormClasses/Settings/AllSettings.cs
@@ -0,0 +1,22 @@
+﻿// 23 11 2020 Created by Tony Horsham 16:17
+
+using FRTForm.BlockTime.Settings;
+using FRTForm.Settings;
+
+namespace Demo.FormClasses.Settings
+{
+    public class AllSettings : IAllSettings
+    {
+        public AllSettings(IApplicationSettings applicationSettings, 
+            CalendarSettings calendarSettings)
+        {
+            ApplicationSettings = applicationSettings;
+            CalendarSettings = calendarSettings;
+        }
+
+        public IApplicationSettings ApplicationSettings { get; }
+        public CalendarSettings CalendarSettings { get; }
+        public CurrentBlockParameters CurrentBlockParameters { get; set; }
+        public bool Is24 { get; set; }
+    }
+}
