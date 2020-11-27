ac468216ba82f78199716341bd808578ddc157ac
Tony Horsham <horshamt@gmail.com>
2020-11-27 11:10:58 +1100

    add bare BT versions of settings

---
+++ FRTForm/BlockTime/Settings/IAllSettingsBT.cs
@@ -0,0 +1,12 @@
+﻿// 27 11 2020 Created by Tony Horsham 11:06
+
+using FRTForm.Settings;
+
+namespace FRTForm.BlockTime.Settings
+{
+    // ReSharper disable once InconsistentNaming
+    public interface IAllSettingsBT : IAllSettings
+    {
+        
+    }
+}
---
+++ FRTForm/BlockTime/Settings/IApplicationSettingsBT.cs
@@ -0,0 +1,12 @@
+﻿// 27 11 2020 Created by Tony Horsham 11:09
+
+using FRTForm.Settings;
+
+namespace FRTForm.BlockTime.Settings
+{
+    // ReSharper disable once InconsistentNaming
+    public interface IApplicationSettingsBT : IApplicationSettings
+    {
+        
+    }
+}
