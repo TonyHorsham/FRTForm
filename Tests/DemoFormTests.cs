a287b502f52cadfd672ed8136c7a22469352e665
Tony Horsham <horshamt@gmail.com>
2020-12-05 17:45:52 +1100

    all elements now covered in .ExtractElements

--- Tests/DemoFormTests.cs
+++ Tests/DemoFormTests.cs
@@ -57,31 +57,41 @@ namespace Tests
         }
         [Test]
         public void Elements_Includes_NamedElements([Values("DisplayOnly", "Close",
-            "DisplayHeader", "TextInput", "Select", "SubmitHeader", "Title", "StartTime", "Duration")] string name)
+            "DisplayHeader", "TextInput", "Select", "SubmitHeader", "TextArea", "Title",
+            "StartTime", "Duration")] string name)
         {
             var element = _testFormElements.FirstOrDefault(fe => fe.Name == name);
             Assert.IsNotNull(element);
             //check that tests do not interfere
             ExtractElements(out var displayOnly, out var closeElement, out var display,
-                out var submit, out var title, 
+                out var input, out var select, out var submit,
+                out var textArea, out var title, 
                 out var start, out var duration, _testFormElements);
             Assert.AreEqual("All elements displayed for styling initially", title.Value);
         }
         #region Utilities
 
         private void ExtractElements(out ButtonElement displayOnly, out CloseElement closeElement,
-            out EditDeleteCloseElement display, out SubmitAndCloseElement submit, 
+            out EditDeleteCloseElement display, out InputElement input, out SelectElement select,
+            out SubmitAndCloseElement submit, out TextAreaElement textArea,
             out TitleElement title, out BlockTimeElement start, out BlockTimeElement duration,
             List<IFormElement> elements)
         {
+            // N.B. Use name of elements rather than inputType, even if only one example of each
             displayOnly = (ButtonElement) elements
                 .FirstOrDefault(fe => fe.Name == "DisplayOnly");
             closeElement = (CloseElement) elements
                 .FirstOrDefault(fe => fe.Name == "Close");
+            input = (InputElement) elements
+                .FirstOrDefault(fe => fe.Name == "TextInput");
+            select = (SelectElement)elements
+                .FirstOrDefault(fe => fe.Name == "Select");
             display = (EditDeleteCloseElement) elements
                 .FirstOrDefault(fe => fe.Name == "DisplayHeader");
             submit = (SubmitAndCloseElement)elements
                 .FirstOrDefault(fe => fe.Name == "SubmitHeader");
+            textArea = (TextAreaElement)elements
+                .FirstOrDefault(fe => fe.Name == "TextArea");
             title = (TitleElement)elements
                 .FirstOrDefault(fe => fe.Name == "Title");
             start = (BlockTimeElement)elements
