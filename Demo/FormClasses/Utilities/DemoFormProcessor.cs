1dd855b3742559bb52df6ba0bdc2460bb97d6d1e
Tony Horsham <horshamt@gmail.com>
2020-12-08 09:52:17 +1100

    move .ExtractElements to formProcessor and make public

--- Demo/FormClasses/Utilities/DemoFormProcessor.cs
+++ Demo/FormClasses/Utilities/DemoFormProcessor.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using FRTForm.BlockTime.Models;
 using FRTForm.Models;
 using FRTForm.Settings;
 using FRTForm.Utilities;
@@ -27,5 +29,34 @@ namespace Demo.FormClasses.Utilities
         }
 
         public event EventHandler ElementsUpdated;
+        // use for tests as well as locally
+        public void ExtractElements(out ButtonElement displayOnly, out CloseElement closeElement,
+            out EditDeleteCloseElement display, out InputElement input, out SelectElement select,
+            out SubmitAndCloseElement submit, out TextAreaElement textArea,
+            out TitleElement title, out BlockTimeElement start, out BlockTimeElement duration,
+            List<IFormElement> elements)
+        {
+            // N.B. Use name of elements rather than inputType, even if only one example of each
+            displayOnly = (ButtonElement)elements
+                .FirstOrDefault(fe => fe.Name == "DisplayOnly");
+            closeElement = (CloseElement)elements
+                .FirstOrDefault(fe => fe.Name == "Close");
+            input = (InputElement)elements
+                .FirstOrDefault(fe => fe.Name == "TextInput");
+            select = (SelectElement)elements
+                .FirstOrDefault(fe => fe.Name == "Select");
+            display = (EditDeleteCloseElement)elements
+                .FirstOrDefault(fe => fe.Name == "DisplayHeader");
+            submit = (SubmitAndCloseElement)elements
+                .FirstOrDefault(fe => fe.Name == "SubmitHeader");
+            textArea = (TextAreaElement)elements
+                .FirstOrDefault(fe => fe.Name == "TextArea");
+            title = (TitleElement)elements
+                .FirstOrDefault(fe => fe.Name == "Title");
+            start = (BlockTimeElement)elements
+                .FirstOrDefault(fe => fe.Name == "StartTime");
+            duration = (BlockTimeElement)elements
+                .FirstOrDefault(fe => fe.Name == "Duration");
+        }
     }
 }
--- Tests/DemoFormTests.cs
+++ Tests/DemoFormTests.cs
@@ -21,7 +21,7 @@ namespace Tests
     public class DemoFormTests
     {
         private IAllSettingsBT _allSettings;
-        private IFormProcessor _testFormProcessor;
+        private DemoFormProcessor _testFormProcessor;
         private List<IFormElement> _testFormElements;
 
         [SetUp]
@@ -45,7 +45,7 @@ namespace Tests
             _allSettings.CurrentBlockParameters = new CurrentBlockParameters(block, calendarDay,
                 "", false, DateTimeOffset.MinValue, DateTimeOffset.MaxValue);
             var formSpec = formSpecs["modalFormSpecs"];
-            _testFormProcessor = formSpec.FormProcessor;
+            _testFormProcessor = (DemoFormProcessor) formSpec.FormProcessor;
             _testFormElements = formSpec.Elements;
         }
 
@@ -63,7 +63,7 @@ namespace Tests
             var element = _testFormElements.FirstOrDefault(fe => fe.Name == name);
             Assert.IsNotNull(element);
             //check that tests do not interfere
-            ExtractElements(out var displayOnly, out var closeElement, out var display,
+            _testFormProcessor.ExtractElements(out var displayOnly, out var closeElement, out var display,
                 out var input, out var select, out var submit,
                 out var textArea, out var title, 
                 out var start, out var duration, _testFormElements);
@@ -71,34 +71,7 @@ namespace Tests
         }
         #region Utilities
 
-        private void ExtractElements(out ButtonElement displayOnly, out CloseElement closeElement,
-            out EditDeleteCloseElement display, out InputElement input, out SelectElement select,
-            out SubmitAndCloseElement submit, out TextAreaElement textArea,
-            out TitleElement title, out BlockTimeElement start, out BlockTimeElement duration,
-            List<IFormElement> elements)
-        {
-            // N.B. Use name of elements rather than inputType, even if only one example of each
-            displayOnly = (ButtonElement) elements
-                .FirstOrDefault(fe => fe.Name == "DisplayOnly");
-            closeElement = (CloseElement) elements
-                .FirstOrDefault(fe => fe.Name == "Close");
-            input = (InputElement) elements
-                .FirstOrDefault(fe => fe.Name == "TextInput");
-            select = (SelectElement)elements
-                .FirstOrDefault(fe => fe.Name == "Select");
-            display = (EditDeleteCloseElement) elements
-                .FirstOrDefault(fe => fe.Name == "DisplayHeader");
-            submit = (SubmitAndCloseElement)elements
-                .FirstOrDefault(fe => fe.Name == "SubmitHeader");
-            textArea = (TextAreaElement)elements
-                .FirstOrDefault(fe => fe.Name == "TextArea");
-            title = (TitleElement)elements
-                .FirstOrDefault(fe => fe.Name == "Title");
-            start = (BlockTimeElement)elements
-                .FirstOrDefault(fe => fe.Name == "StartTime");
-            duration = (BlockTimeElement)elements
-                .FirstOrDefault(fe => fe.Name == "Duration");
-        }
+        
 
         #endregion
     }
