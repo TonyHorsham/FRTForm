243bdd9d419b461c45884b1a37cef9f4f24f314c
Tony Horsham <horshamt@gmail.com>
2021-01-01 13:09:29 +1100

    add todo

--- FRTForm/BlockTime/Models/CalendarDay.cs
+++ FRTForm/BlockTime/Models/CalendarDay.cs
@@ -18,5 +18,12 @@ namespace FRTForm.BlockTime.Models
             LocationId = locationId;
             DayDate = dayDate.Date;// cannot rely on caller just sending the date
         }
+        //TODO consider a repository of these, so only one in the system rather than one for each user
+        //Also consider including a List<Block> and raising events for any block changes (including addition and deletion)
+        //Day.razor.cs instances to subscribe to the event, so that any changes result in all users getting the update
+
+        //block addition on first come best dressed, so 'second' user with a block with overlapping time to get 'not possible' message
+        //  this almost certainly means Day should raise an event so that BlockForm can subscribe.
+        //  similar logic applies to changing block times - the limits may change before submit
     }
 }
