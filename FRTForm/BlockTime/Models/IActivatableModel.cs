46031fbfaf74e32525b44d87a027b6d16b2fb14f
Tony Horsham <horshamt@gmail.com>
2021-01-09 18:18:32 +1100

    setup and use IAuditedModel and IActivatableModel

---
+++ FRTForm/BlockTime/Models/IActivatableModel.cs
@@ -0,0 +1,9 @@
+﻿// 09 01 2021 Created by Tony Horsham 18:12
+
+namespace FRTForm.BlockTime.Models
+{
+    public interface IActivatableModel
+    {
+        public bool IsActive { get; set; }
+    }
+}
---
+++ FRTForm/BlockTime/Models/IAuditedModel.cs
@@ -0,0 +1,14 @@
+﻿// 09 01 2021 Created by Tony Horsham 18:10
+
+using System;
+
+namespace FRTForm.BlockTime.Models
+{
+    public interface IAuditedModel
+    {
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public string LastModifiedUserId { get; set; }
+        public DateTimeOffset? LastModifiedDate { get; set; }
+    }
+}
